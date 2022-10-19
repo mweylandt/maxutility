f862c4a8f5c348091e85bc6749b3c981c3532c71
Max Weylandt <mweylandt@gmail.com>
2022-10-19 10:31:19 +0200

    added function to reverse codings in survey

--- R/others.R
+++ R/others.R
@@ -1,5 +1,6 @@
 #' Add wordcount and character count to dataframe
 #' @export
+#' @name add_textcounts
 #' @param highlight a regex string to be highlighted
 #' @param highlight_style Interpret regex directly or, if "stem" is specified,
 #'        highlight whole words beginning with the stem specified
@@ -25,6 +26,39 @@ add_textcounts <- function (x, text_col, word = TRUE, char = TRUE) {
 
   return(x)
 }
+
+
+#' Reverse codings of items (such as in a survey)
+#' @export
+#' @name reverse
+#' @examples
+#' ideology <-  c(1, 4, 4, 5, 2, 2, 2, 4, 4, 6, 2, 4, 5, 7, 4, 1, 2, 3, 2, 3, 8)
+#' reverse(ideology)
+#' library(dplyr)
+#' df <- data.frame(id = seq(1:length(ideology)),ideology )
+#' df %>% mutate(ideology_r = reverse(ideology))
+
+reverse <- function (x) {
+  for (i in 1:length(x)) {
+    x[i] <- abs((max(x)+1) - x[i])
+  }
+  return(x)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
