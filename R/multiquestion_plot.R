9893beffb562ad1fd3d122e2c33b6a83a02738dc
Max Weylandt <mweylandt@gmail.com>
2022-10-22 13:24:58 +0200

    added function to create small-multiples plot from several survey questions

---
+++ R/multiquestion_plot.R
@@ -0,0 +1,63 @@
+#' Build a multi-graph plot that combines plots for each of the questions
+#' you want to compare
+#' @export
+#' @param vars a vector of column names
+#' @param extract_labels if the columns have labels, you can extract them and
+#' make them the title of each plot
+#' @param labels manually specify labels
+#' @param fill_var Variable to be used as fill aesthetic in ggplot
+#' @param title_text overall graph title
+#' multiquestion_plot()
+
+
+
+multiquestion_plot <- function(x, vars = NULL,
+                               extract_labels=TRUE,
+                               labels = NULL,
+                               fill_var = NULL,
+                               title_text = NULL) {
+
+  var_names <- vars
+
+  # get labels
+
+  if(extract_labels == TRUE) {
+    var_labels <- x %>% select(all_of(var_names))%>%
+      lapply( attr, "label") %>% unlist() %>% unname()
+
+  } else {
+    var_labels <- labels
+  }
+
+  # get min and max
+  x_min <- x %>% select(all_of(var_names)) %>% min()
+  x_max <- x %>% select(all_of(var_names)) %>% max()
+
+
+
+  graphs <- list()
+
+  for (i in 1:length(var_names)) {
+    graphs[[i]] <- ggplot(x) +
+      aes_string(x=var_names[i], fill = fill_var) +
+      geom_bar(aes(y = ..prop..),  stat="count",
+               position=position_dodge2(preserve = "single"))+
+      labs(title=var_labels[i])+
+      scale_x_continuous(breaks = seq(x_min, x_max, by = 1),
+                         limits = c(x_min-1,x_max+1))+
+
+      #theme(legend.position="none")+
+      theme_bw()+
+      theme(axis.title.x = element_blank(),
+            #axis.text.x = element_blank()
+            text = element_text(size=10)
+      )
+
+  }
+
+  wrap_plots(graphs, ncol = 2) +
+    plot_layout(guides = 'collect')+
+    plot_annotation(title=title_text)
+
+
+}
--- R/others.R
+++ R/others.R
@@ -32,22 +32,52 @@ add_textcounts <- function (x, text_col, word = TRUE, char = TRUE) {
 #' @export
 #' @name reverse
 #' @examples
+#' test <- c(1,NA,3)
+#' reverse(test, min=1, max=3)
+#'
+#' test2 <- c(1,2,3,4)
+#' reverse(test2, min=1, max=4)
+#'
+#' test3 <- c(0,1,2)
+#' reverse(test3, min=0, max=2)
+#'
+#' test4 <- c(-1,0,1,2,3)
+#' reverse(test4, min=-1, max=3)
 #' ideology <-  c(1, 4, 4, 5, 2, 2, 2, 4, 4, 6, 2, 4, 5, 7, 4, 1, 2, 3, 2, 3, 8)
 #' reverse(ideology)
 #' library(dplyr)
+#' ideology <-  c(1, 4, 4, 5, 2, 2, 2, 4, 4, 6, 2, 4, 5, 7, 4, 1, 2, 3, 2, 3)
 #' df <- data.frame(id = seq(1:length(ideology)),ideology )
-#' df %>% mutate(ideology_r = reverse(ideology))
+#' df %>% mutate(ideology_r = reverse(ideology, min=1, max=7))
 
-reverse <- function (x) {
-  for (i in 1:length(x)) {
-    x[i] <- abs((max(x)+1) - x[i])
+reverse <- function (x, min = NULL, max = NULL) {
+
+  # create named vector with old values as names
+  # and new values as values
+  # then, loop through all the values and read the
+  # correct new value that corresponds to the old
+  # value (i.e vector name)
+
+  newvalues <- rev(seq(from=min, to=max))
+  names(newvalues) <- seq(from=min, to=max) # names with OG labels
+
+  x_r <- rep(NA, length(x))
+
+  for (i in seq(1,length(x))) {
+
+    if(is.na(x[i])){ # have to handle NA manually
+      x_r[i] <- NA
+
+    } else {
+
+    index <- which(names(newvalues)==x[i])
+    x_r[i] <- newvalues[[index]]
+    }
   }
-  return(x)
+
+  return(x_r)
+
 }
-
-
-
-
 
 
 
